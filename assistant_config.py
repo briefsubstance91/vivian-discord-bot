baa0942c7d5f39a1b086bccecc4c05a3f0b06233
briefsubstance91 <bgelineau@gmail.com>
2025-05-23 10:34:07 -0400

    Update assistant_config.py

--- assistant_config.py
+++ assistant_config.py
@@ -1,2 +1,7 @@
-ASSISTANT_ID =asst_faNNrSBJG4iJAyqroxHzuwR6
-THREAD_ID =thread_G6ptoRQ5MDYPJw3lYAToZ9dT
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Must be called before using os.getenv()
+
+ASSISTANT_ID = os.getenv("asst_faNNrSBJG4iJAyqroxHzuwR6")
+THREAD_ID = os.getenv("thread_G6ptoRQ5MDYPJw3lYAToZ9dT")
